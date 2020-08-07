f129e165382ecf06910ebea2c8321d2bab951b4f
Miguel De La Cruz <iscrusman@gmail.com>
2020-08-06 23:16:09 -0700

    data sets
    data sets

--- admin-app/src/views/DataSetList/data.js
+++ admin-app/src/views/DataSetList/data.js
@@ -8,7 +8,7 @@ export default [
             "Global coronavirus (COVID-19) data, including US county level data, tracking the number of confirmed cases, deaths, recoveries and individuals tested for the virus. From April 11, 2020 data will be updated daily to match the update frequency at source.",
         imageUrl: "https://d1ewbp317vsrbd.cloudfront.net/5c1bf2cf-c411-472f-9559-f4e4b7b5948e.png",
         totalDownloads: "594",
-        updatedAt: "27/03/2019",
+        updatedAt: "27/03/2020",
     },
     {
         id: uuid(),
@@ -17,7 +17,7 @@ export default [
             "Coronavirus (COVID-19) data that has been gathered and unified from trusted sources. This data is provided to the public by Salesforce, MuleSoft, and Tableau at no cost to help you make make better decisions, fast.        ",
         imageUrl: "https://d1ewbp317vsrbd.cloudfront.net/bf993fee-a501-4607-9647-4adb4ac9008e.png",
         totalDownloads: "625",
-        createdAt: "31/03/2019",
+        createdAt: "31/03/2020",
     },
     {
         id: uuid(),
@@ -26,7 +26,7 @@ export default [
             "Limited sample of IMDb's essential data for every movie, TV and OTT series, and video game title as well as performers and creators. Includes IMDb's 1-10 star rating, a daily-computed average of votes from IMDb's global audience of 250 million visitors. For free access please email...        ",
         imageUrl: "https://d1ewbp317vsrbd.cloudfront.net/e56861ba-fc04-41de-ac7e-fcefe9dc6ef9.png",
         totalDownloads: "857",
-        createdAt: "03/04/2019",
+        createdAt: "03/04/2020",
     },
     {
         id: uuid(),
@@ -35,7 +35,7 @@ export default [
             "Federal Reserve Board's data download program provides free and open access to various financial and economic data. Treasury yield refers to the return on an investment in a U.S. government debt obligation, such as a bill, note or bond.        ",
         imageUrl: "https://d1ewbp317vsrbd.cloudfront.net/fa051f49-9c08-41ce-a05f-a4f18569229a.png",
         totalDownloads: "406",
-        createdAt: "04/04/2019",
+        createdAt: "04/04/2020",
     },
     {
         id: uuid(),
@@ -44,7 +44,7 @@ export default [
             "This dataset contains monthly unemployment rate by state from 2015 to 2020. This dataset is updated each month as and when data is made available by Bureau of Labor Statistics        ",
         imageUrl: "https://d1ewbp317vsrbd.cloudfront.net/9c7ac4c3-c162-4158-a64b-3e6e168443b4.PNG",
         totalDownloads: "835",
-        createdAt: "04/04/2019",
+        createdAt: "04/04/2020",
     },
     {
         id: uuid(),
@@ -53,6 +53,6 @@ export default [
             "In response to the COVID-19 pandemic, BreezoMeter is now providing the previous month's hourly Air Quality information for free for COVID-19 related research - for 2132 geolocation in 1352 cities in the USA. The data is in a newline JSON format where every line is a BreezoMeter API result. If you...        ",
         imageUrl: "https://d1ewbp317vsrbd.cloudfront.net/074ae4f5-f6f4-43ab-8ad5-64cc04f505e5.png",
         totalDownloads: "835",
-        createdAt: "04/04/2019",
+        createdAt: "04/04/2020",
     },
 ];
